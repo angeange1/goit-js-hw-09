4a3a2879bc82c7ebfde9212b661e2d1bac3270ff
angeange1 <149048056+angeange1@users.noreply.github.com>
2024-03-14 23:34:55 +0900

    commit1

---
+++ src/js/2-form.js
@@ -0,0 +1,16 @@
+// const form = document.querySelector(".feedback-form")
+// const email = form.elements.email.value ??
+
+// const currentLocalStorage = localStorage.getItem("feedback-form-state")
+// const parsedFormState = JSON.parse(currentLocalStorage)
+// if(parsedFormState.email === "" || par){} ??
+
+// form.addEventListener("input", addToLocal)
+
+// function addToLocal(event) {
+//     const values = {
+//     email: "${event.target.elements.email.value}"
+//     message: "${event.target.elements.message.value}"
+// }
+//     localStorage.setItem("feedback-form-state", JSON.stringify(values))
+// }
